cd123a43483de1611457615fa8c4c3c5feb8bbea
Tim Cross <theophilusx@gmail.com>
2024-01-11 14:04:13 +1100

    Add some tooling config files

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js-ts-mode . ((flymake-eslint-executable-name . "~/git/ems-ssip/node_modules/.bin/eslint"))))
